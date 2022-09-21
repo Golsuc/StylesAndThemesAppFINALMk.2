d837263659b0f071a0987ccb6be76fbafacde44b
Golsuc <90780174+Golsuc@users.noreply.github.com>
2022-09-20 22:45:18 -0500

    Complete Project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+        ConstraintLayout constraintLayout = findViewById(R.id.appBackround);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
+
+    }
+    public void switchedScreens(View view){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+
+}
